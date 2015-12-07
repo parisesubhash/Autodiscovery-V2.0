32a399179a40226f0eeb5eb795273d09df77b3ad
GS <hemanth.gs@capgemini.com>
2015-12-07 16:48:31 +0530

    python_folder

---
+++ python/generate_ps_json.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+
+blist=[str(sys.argv[1]),'%cpu','%mem']
+var='ps -p %s -o %s,%s,cmd' %(blist[0],blist[1],blist[2])
+proc = subprocess.Popen(var, stdout=subprocess.PIPE,shell=True)
+output = proc.stdout.read()
+alist=output.split(' ',5)
+final=alist[3:6]
+dict={'PID':sys.argv[1],'CPU':alist[3],'RAM':alist[4],'Process':alist[5]}
+
+print final
+print dict
