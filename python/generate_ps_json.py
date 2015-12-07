a980b9bf2f71061566832b276327b3491a5e94b0
GS <hemanth.gs@capgemini.com>
2015-12-07 16:51:09 +0530

    python_file edit

--- python/generate_ps_json.py
+++ python/generate_ps_json.py
@@ -6,8 +6,6 @@ var='ps -p %s -o %s,%s,cmd' %(blist[0],blist[1],blist[2])
 proc = subprocess.Popen(var, stdout=subprocess.PIPE,shell=True)
 output = proc.stdout.read()
 alist=output.split(' ',5)
-final=alist[3:6]
 dict={'PID':sys.argv[1],'CPU':alist[3],'RAM':alist[4],'Process':alist[5]}
 
-print final
 print dict
